b5740fe68350bd6e2452c5c7508b1237d5f9ecd7
habdog <ivansitdikov18@gmail.com>
2026-01-29 20:47:20 +0300

    Тестирование CheckingAccount в main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "CheckingAccount.h"
 using namespace std;
 int main() {
-    cout << "Hello!";
-    return 0;
+
+     CheckingAccount c(184393.95, 0, 4.87);
+     c.deposit(10000);
+     c.withdraw(34000);
+     c.printInfo();
+     c.resetTransactions();
+     c.printInfo();
+
 }
